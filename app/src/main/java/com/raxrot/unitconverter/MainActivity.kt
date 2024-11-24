dd130bc281664dbbf000e390a9a3c1285d2ed919
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-24 18:37:57 +0000

    add Spacing

--- app/src/main/java/com/raxrot/unitconverter/MainActivity.kt
+++ app/src/main/java/com/raxrot/unitconverter/MainActivity.kt
@@ -3,10 +3,13 @@ package com.raxrot.unitconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -16,9 +19,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.raxrot.unitconverter.ui.theme.UnitConverterTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,10 +44,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(){
-    Column {
+    Column(
+        modifier=Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
         //How UI elements will be stacked below each Other
         Text("Unit Converter")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(value = "", onValueChange ={} )
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
             //How UI elements will be stacked next each Other
@@ -61,6 +74,7 @@ fun UnitConverter(){
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result:")
     }
 }
